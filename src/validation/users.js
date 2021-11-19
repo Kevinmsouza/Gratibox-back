89bdaf2f0c9efd710fae1523d2314b4434225e50
Kevinmsouza <kevinm.souza@hotmail.com>
2021-11-19 13:24:34 -0400

    created user body validation

---
+++ src/validation/users.js
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+
+function validateUser(user) {
+    const userSchema = Joi.object({
+        username: Joi.string().min(3).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(4).required(),
+    });
+    return !!userSchema.validate(user).error;
+}
+
+export default {
+    validateUser,
+};
